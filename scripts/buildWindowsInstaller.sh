47f853b557de09c52c75ccf67653495ef60ae611
Jussi Lind <jussi.lind@iki.fi>
2018-07-11 15:57:32 +0300

    Use QMake for Windows build as CMake has problems with static Qt5

--- scripts/buildWindowsInstaller.sh
+++ scripts/buildWindowsInstaller.sh
@@ -2,10 +2,9 @@
 
 # Builds NSIS installer for Windows in Docker
 
-CMAKE=/opt/mxe/usr/bin/i686-w64-mingw32.static-cmake
-CPACK=/opt/mxe/usr/bin/i686-w64-mingw32.static-cpack
+QMAKE=/opt/mxe/usr/bin/i686-w64-mingw32.static-qmake-qt5
 
-CMD="export LANG=en_US.UTF-8 && export LC_ALL=en_US.UTF-8 && export PATH='$PATH':/opt/mxe/usr/bin && cd /heimer && mkdir -p build-windows-docker && cd build-windows-docker && ${CMAKE} -DCMAKE_BUILD_TYPE=Release .. && ${CMAKE} --build . --target all -- -j3 && ${CPACK} -G NSIS64"
+CMD="export LANG=en_US.UTF-8 && export LC_ALL=en_US.UTF-8 && export PATH='$PATH':/opt/mxe/usr/bin && cd /heimer && mkdir -p build-windows-docker && cd build-windows-docker && ${QMAKE} .. && make -j3"
 
 if [ -f /.dockerenv ]; then
     echo "Script inside Docker"

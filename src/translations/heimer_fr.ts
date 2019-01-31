2b89f5929085f12a4e43bccea25b13f6c5ac4fde
olielvewen <olivier@openshot.org>
2019-01-31 13:47:30 +0100

    added the heimer_fr.ts file

---
+++ src/translations/heimer_fr.ts
@@ -0,0 +1,298 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>AboutDlg</name>
+    <message>
+        <location filename="../aboutdlg.cpp" line="28"/>
+        <source>About </source>
+        <translation>Aide </translation>
+    </message>
+    <message>
+        <location filename="../aboutdlg.cpp" line="43"/>
+        <source> is licenced under </source>
+        <translation> est sous licence </translation>
+    </message>
+</context>
+<context>
+    <name>Application</name>
+    <message>
+        <location filename="../application.cpp" line="150"/>
+        <source>Heimer Files</source>
+        <translation>Heimer Fichiers</translation>
+    </message>
+    <message>
+        <location filename="../application.cpp" line="228"/>
+        <source>Open File</source>
+        <translation>Ouvrir un fichier</translation>
+    </message>
+    <message>
+        <location filename="../application.cpp" line="257"/>
+        <source>Failed to save file.</source>
+        <translation>Erreur lors de la sauvegarde.</translation>
+    </message>
+    <message>
+        <location filename="../application.cpp" line="275"/>
+        <source>Save File As</source>
+        <translation>Enregistrer sous</translation>
+    </message>
+    <message>
+        <location filename="../application.cpp" line="291"/>
+        <source>File &apos;</source>
+        <translation>Fichier.&apos;</translation>
+    </message>
+    <message>
+        <location filename="../application.cpp" line="291"/>
+        <source>&apos; saved.</source>
+        <translation>&apos;sauvegarder.</translation>
+    </message>
+    <message>
+        <location filename="../application.cpp" line="297"/>
+        <source>Failed to save file as &apos;</source>
+        <translation>Erreur d&apos;enregistrement sous&apos;</translation>
+    </message>
+    <message>
+        <location filename="../application.cpp" line="340"/>
+        <source>The mind map has been modified.</source>
+        <translation>La carte heuristique a été modifié.</translation>
+    </message>
+    <message>
+        <location filename="../application.cpp" line="341"/>
+        <source>Do you want to save your changes?</source>
+        <translation>Voulez-vous enregistrer vos modifications?</translation>
+    </message>
+</context>
+<context>
+    <name>EditorView</name>
+    <message>
+        <location filename="../editorview.cpp" line="60"/>
+        <source>Set background color</source>
+        <translation>Affichage de la couleur d&apos;arrière-plan</translation>
+    </message>
+    <message>
+        <location filename="../editorview.cpp" line="65"/>
+        <source>Create floating node</source>
+        <translation>Création d&apos;un noeud déplacable</translation>
+    </message>
+    <message>
+        <location filename="../editorview.cpp" line="78"/>
+        <source>Set node color</source>
+        <translation>Affichage de la couleur du noeud</translation>
+    </message>
+    <message>
+        <location filename="../editorview.cpp" line="88"/>
+        <source>Set text color</source>
+        <translation>Affichage de la couleur du texte</translation>
+    </message>
+    <message>
+        <location filename="../editorview.cpp" line="98"/>
+        <source>Delete node</source>
+        <translation>Supprimer un noeud</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="57"/>
+        <source>Redo</source>
+        <translation>Rétablir</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="72"/>
+        <source>Undo</source>
+        <translation>Défaire</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="87"/>
+        <source>&amp;Edit</source>
+        <translation>&amp;Edition</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="95"/>
+        <source>Set background color...</source>
+        <translation>Affichage de la couleur d&apos;arrière-plan...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="118"/>
+        <source>Edge width:</source>
+        <translation>Taille du contour:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="143"/>
+        <source>Text size:</source>
+        <translation>Taille du texte:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="168"/>
+        <source>Grid size:</source>
+        <translation>Taille de la grille:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="185"/>
+        <source>&amp;File</source>
+        <translation>&amp;Fichier</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="188"/>
+        <source>&amp;New...</source>
+        <translation>&amp;Nouveau...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="196"/>
+        <source>&amp;Open...</source>
+        <translation>&amp;Ouvrir...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="206"/>
+        <source>&amp;Save</source>
+        <translation>&amp;Enregistrer</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="215"/>
+        <source>&amp;Save as...</source>
+        <translation>&amp;Enregistrer sous...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="226"/>
+        <source>&amp;Export to PNG image...</source>
+        <translation>&amp;Exporter au format PNG...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="236"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Quitter</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="246"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Aide</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="249"/>
+        <source>&amp;About</source>
+        <translation>&amp;A propos d&apos; Heimer</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="254"/>
+        <source>About &amp;Qt</source>
+        <translation>A propos de &amp;Qt</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="272"/>
+        <source>&amp;View</source>
+        <translation>&amp;Vue</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="275"/>
+        <source>Zoom In</source>
+        <translation>Zoom avant</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="281"/>
+        <source>Zoom Out</source>
+        <translation>Zoom arrière</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="289"/>
+        <source>&amp;Zoom To Fit</source>
+        <translation>&amp;Zoom ajusté à la taille de la fenêtre</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="327"/>
+        <source>New File</source>
+        <translation>Nouveau fichier</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="330"/>
+        <source>Not Saved</source>
+        <translation>Pas enregistré</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="408"/>
+        <source>About Qt</source>
+        <translation>A propos de Qt</translation>
+    </message>
+</context>
+<context>
+    <name>PngExportDialog</name>
+    <message>
+        <location filename="../pngexportdialog.cpp" line="35"/>
+        <source>Export to PNG Image</source>
+        <translation>Exporter au format PNG</translation>
+    </message>
+    <message>
+        <location filename="../pngexportdialog.cpp" line="41"/>
+        <source>Export As</source>
+        <translation>Exporter vers</translation>
+    </message>
+    <message>
+        <location filename="../pngexportdialog.cpp" line="43"/>
+        <source>PNG Files</source>
+        <translation>Fichiers PNG</translation>
+    </message>
+    <message>
+        <location filename="../pngexportdialog.cpp" line="115"/>
+        <source>Couldn&apos;t write to</source>
+        <translation>Impossible d&apos;écrire</translation>
+    </message>
+    <message>
+        <location filename="../pngexportdialog.cpp" line="147"/>
+        <source>Filename</source>
+        <translation>Nom de fichier</translation>
+    </message>
+    <message>
+        <location filename="../pngexportdialog.cpp" line="153"/>
+        <source>Export as..</source>
+        <translatorcomment>A voir- exporter en tant que</translatorcomment>
+        <translation>Exporter vers..</translation>
+    </message>
+    <message>
+        <location filename="../pngexportdialog.cpp" line="157"/>
+        <source>Image Size</source>
+        <translation>Taille de l&apos;image</translation>
+    </message>
+    <message>
+        <location filename="../pngexportdialog.cpp" line="159"/>
+        <source>Width (px):</source>
+        <translation>Largeur (px):</translation>
+    </message>
+    <message>
+        <location filename="../pngexportdialog.cpp" line="165"/>
+        <source>Height (px):</source>
+        <translation>Hauteur (px):</translation>
+    </message>
+    <message>
+        <location filename="../pngexportdialog.cpp" line="173"/>
+        <source>Background</source>
+        <translation>Arrière-plan</translation>
+    </message>
+    <message>
+        <location filename="../pngexportdialog.cpp" line="176"/>
+        <source>Transparent background</source>
+        <translation>Transparence de l&apos;arrière-plan</translation>
+    </message>
+    <message>
+        <location filename="../pngexportdialog.cpp" line="190"/>
+        <source>Cancel</source>
+        <translation>Abandonner</translation>
+    </message>
+    <message>
+        <location filename="../pngexportdialog.cpp" line="193"/>
+        <source>Export</source>
+        <translation>Exporter</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../reader.cpp" line="28"/>
+        <source>Cannot open file: &apos;</source>
+        <translation>Impossible d&apos;ouvrir le fichier:&apos;</translation>
+    </message>
+    <message>
+        <location filename="../reader.cpp" line="35"/>
+        <source>Corrupted file: &apos;</source>
+        <translation>Fichier corrompu:&apos;</translation>
+    </message>
+</context>
+</TS>
